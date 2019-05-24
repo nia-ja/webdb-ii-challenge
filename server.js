cf289c4acc2f06556de67cdec68e452548b415ca
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-23 22:21:43 -0700

    Added an wired up bears table, added basic GET route for /api/bears to test that all works fine

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const helmet = require('helmet');
 
 const ZoosRouter = require('./zoos/zoosRouter.js');
+const BearsRouter = require('./bears/bearsRouter.js');
 
 const server = express();
 server.use(express.json());
 server.use(helmet());
 
 server.use('/api/zoos', ZoosRouter);
+server.use('/api/bears', BearsRouter);
+
 
 server.get('/', (req, res) => {
     res.send(`<h2>Zoos API!</h2>`)
